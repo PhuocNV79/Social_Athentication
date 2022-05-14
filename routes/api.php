cc713b2d3d2df71318a1423e6431436807e22c4c
Nguyen Van Phuoc <nguyenvanphuoc@Nguyens-MacBook-Pro.local>
2022-05-14 15:33:32 +0700

    save source

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::get('login/twitter', [LoginController::class, 'redirectToTwitter']);
+Route::get('twitter/callback', [LoginController::class, 'handleTwitterCallback']);
+Route::get('/home', function () {
+    return 'User is logged in';
 });
