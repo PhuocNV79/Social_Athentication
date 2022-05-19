ffee57e4bb33986f8a3fe491e2f43d4ef68fef8e
Nguyen Van Phuoc <nguyenvanphuoc@Nguyens-MacBook-Pro.local>
2022-05-19 10:51:34 +0700

    feature login with twitter run ok

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'twitter' => [
         'client_id' => env('TWITTER_API_KEY'),
         'client_secret' => env('TWITTER_API_SECRET_KEY'),
-        'redirect' => env('TWITTER_CALLBACK_URL'),
+        'redirect' => env('TWITTER_CALLBACK_URL', 'http://laravel.test/twitter/callback'),
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,10 @@ use App\Http\Controllers\LoginController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['middleware'=>'web'], function(){
-    Route::get('login/{provider}', [LoginController::class, 'redirectToProvider']);
-    Route::get('{provider}/callback', [LoginController::class, 'handleProviderCallback']);
-    Route::get('/home', function () {
-        return 'User is logged in';
-    });
-});
-
+//Route::group(['middleware'=>'web'], function(){
+//    Route::get('login/{provider}', [LoginController::class, 'redirectToProvider']);
+//    Route::get('{provider}/callback', [LoginController::class, 'handleProviderCallback']);
+//    Route::get('/home', function () {
+//        return 'User is logged in';
+//    });
+//});
